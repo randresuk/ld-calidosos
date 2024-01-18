48804362aaa2f8ba2984de862bba286eaf07ed5f
RICARDO ANDRES URREGO KUIRU <ing.rauk@gmail.com>
2024-01-18 18:05:57 -0500

    :wrench: fix routing otp

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,13 @@ import { Route, RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { HomeComponent } from "./home/home.component";
+import { OtpValidationComponent } from "./otpValidation/otpValidation.component";
 
 const appRoutes: Routes = [
   { path: "", component: LoginComponent, pathMatch: "full" },
   { path: "home", component: HomeComponent, pathMatch: "full" },
   { path: "login", component: LoginComponent, pathMatch: "full" },
-  { path: "register", component: RegisterComponent, pathMatch: "full" }
+  { path: "register", component: RegisterComponent, pathMatch: "full" },
+  { path: "otp-validation", component: OtpValidationComponent, pathMatch: "full" }
 ];
 export const routing = RouterModule.forRoot(appRoutes);
